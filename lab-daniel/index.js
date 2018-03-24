e443cf9b5b3ef6aea94e92f6559803337bda7cf1
daniel <dpillay03@gmail.com>
2018-03-23 18:14:58 -0700

    server is running on port 3000

--- lab-daniel/index.js
+++ lab-daniel/index.js
@@ -1,3 +1,12 @@
 'use strict'
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`http://localhost:${PORT}`);
+});
