fca0c23201e5439f8ba0a48ea425825fa38bb8ed
dknore <darcy.knore@gmail.com>
2018-03-24 16:38:28 -0700

    all routes working, all tests passed

--- lab-16-darcy/model/user.js
+++ lab-16-darcy/model/user.js
@@ -1,22 +1,41 @@
 'use strict';
 
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const User = new mongoose.Schema({
-  username: {type: String, unique: true, required: true},
-  email: {type: String, unique: true, required: true},
-  password: {type: String, required: true}
+  username: {
+    type: String, 
+    unique: true, 
+    required: true
+  },
+  email: {
+    type: String, 
+    unique: true, 
+    required: true
+  },
+  password: {
+    type: String, 
+    required: true}
 });
+
+User.methods.verifyPassword = function(password) {
+  console.log('inside verifyPassword', password);
+  return bcrypt.compare(password, this.password);
+};
 
 User.pre('save', function(next) {
   if (this.isNew) {
     console.log('New user: ', this);
-    bcrypt.hash(this.password, 10, (err, hash) => {
-      this.password = hash;
-      next();
-    });
+    
+    bcrypt.hash(this.password, 10)
+      .then(hash => {
+        console.log('hash=', hash);
+        this.password = hash;
+        next();
+      })
+      .catch(err => next(err));
   } else {
-    console.log('Old user: ', this);
     next();
   }
 });
--- lab-16-darcy/server.js
+++ lab-16-darcy/server.js
@@ -1,42 +1,19 @@
 'use strict';
 
 const express = require('express');
-// const debug = require('debug')('lab-16-darcy:server');
-// const dotenv = require('dotenv');
-const mongoose = require('mongoose');
-const morgan = require('morgan');
-const cors = require('cors');
-
-const app = express();
-mongoose.connect('mongodb://localhost/16-basic-auth');
-
-app.use(cors());
-app.use(morgan('dev'));
-
-app.get('/', (req, res) => {
-  res.send('Welcome! Try to access <a href="/secret">/secret</a> to see a secret recipe!');
-});
-    
-app.get('/secret', (req, res) => {
-  let authHeader = req.get('Authorization');
-  console.log('header:', authHeader);
-  if (!authHeader) {
-    res.status(401);
-    res.send('Must provide username/password');
-    return;
-  } 
-    
-  let payload = authHeader.split('Basic ')[1];
-  let decoded = Buffer.from(payload, 'base64').toString();
-  let [username, password] = decoded.split(':');
-  console.log('credentials:', username, password);
-    
-  res.send('Secret Recipe.');
-});
 
 const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/lab-16-user');
+
+const router = require('./route/routes.js');
+
+app.use('/api', router);
+
 const server = app.listen(PORT, () => {
-//   debug(`Listening on: ${PORT}`);
   console.log('Listening on http://localhost: ' + PORT);
 });
 
